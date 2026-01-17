d065365911f68f985cc490ab2bdbfc1df2a91d2d
Thimira Vishwa <vishwathimira2@gmail.com>
2026-01-17 21:38:38 +0000

    Updated

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a controlled API to the renderer process (your React app).
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize: () => ipcRenderer.send('window:minimize'),
-  maximize: () => ipcRenderer.send('window:maximize'),
-  close: () => ipcRenderer.send('window:close'),
-});
